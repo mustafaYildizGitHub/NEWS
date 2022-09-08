97367926b2dc567466bac51f812fd68a3d25dbdb
mustafa yildiz <myildiz19@ku.edu.tr>
2022-09-08 15:51:57 +0300

    Initial Commit

---
+++ NEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NEWS
+//
+//  Created by mustafa yildiz on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
